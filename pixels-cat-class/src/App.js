1c0aae976fff926f7c60d4fcfea46855cbf8a171
effs12 <fredericklopez12@gmail.com>
2022-08-16 18:35:04 -0400

    fetch and display the users api

--- pixels-cat-class/src/App.js
+++ pixels-cat-class/src/App.js
@@ -1,16 +1,39 @@
 import * as React from 'react';
 import {
-  Typography,
   Box,
 } from '@mui/material'
 
 import BaseLayout from './modules/layouts/base-layout';
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      cats : [],
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return { cats : users}
+    }, 
+    () => {
+      console.log(this.state)
+    }))
+  }
+
   render(){
+    const { cats } = this.state;
+
     return (
       <BaseLayout>
-        <Box component="section" sx={{ py: 4, height: '1500px' }}></Box>
+        <Box component="section" sx={{ py: 4, height: '1500px' }}>
+        {cats?.map((cat) => (
+            <h1 key={cat.id}>{cat.name}</h1>
+        ))}
+        </Box>
       </BaseLayout>
     )
   }
