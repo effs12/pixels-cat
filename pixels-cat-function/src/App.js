509ab7ad5345d7b858b901ee029bfaca09def5d1
effs12 <fredericklopez12@gmail.com>
2022-08-19 17:03:14 -0400

    Getting the users from API using react Hook

--- pixels-cat-function/src/App.js
+++ pixels-cat-function/src/App.js
@@ -2,10 +2,22 @@ import * as React from 'react';
 import BaseLayout from './modules/layouts/base-layout';
 
 const App = () => {
-    return (
-      <BaseLayout>
-      </BaseLayout>
-    )
+  const [cats, setCats] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setCats(users));
+  },[])
+
+
+  return (
+    <BaseLayout>
+    {cats.map((cat) => (
+      <h1 key={cat.id}>{cat.name}</h1>
+    ))}
+    </BaseLayout>
+  )
 }
 
 export default App;
