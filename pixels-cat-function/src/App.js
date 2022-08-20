e0705f529f651bfadd110add243cb8785534e08e
effs12 <fredericklopez12@gmail.com>
2022-08-19 22:07:13 -0400

    filtered the users

--- pixels-cat-function/src/App.js
+++ pixels-cat-function/src/App.js
@@ -2,9 +2,12 @@ import * as React from 'react';
 import BaseLayout from './modules/layouts/base-layout';
 
 import CardList from './components/card-list.component';
+import SearchBox from './components/search-box.component';
 
 const App = () => {
+  const [searchField, setSearchField] = React.useState('');
   const [cats, setCats] = React.useState([])
+  const [filteredCats, setFilterCats] = React.useState(cats);
 
   React.useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -12,10 +15,28 @@ const App = () => {
     .then((users) => setCats(users));
   },[])
 
+  React.useEffect(() => {
+    const newFilteredCats = cats.filter((cat) => {
+      return cat.name.toLowerCase().includes(searchField)
+    })
+
+    setFilterCats(newFilteredCats)
+
+  },[searchField,cats])
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <BaseLayout>
+      <SearchBox
+        placeholder="Search Pixels Cat Here..."
+        onChangeHandler={onSearchChange}
+      />
       <CardList 
-        cats={cats}
+        cats={filteredCats}
       />
     </BaseLayout>
   )
